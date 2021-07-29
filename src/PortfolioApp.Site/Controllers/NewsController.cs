b1b1390c54f3084d83687c250418c034cf963d6e
Yasinovskiy Egor <yasinovskiy.egor1@gmail.com>
2021-07-29 15:29:47 +0300

    Реализовал добавление и удаление новостей

---
+++ src/PortfolioApp.Site/Controllers/NewsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using PortfolioApp.Site.Models;
+using PortfolioApp.Site.Models.ViewModels;
+
+namespace PortfolioApp.Site.Controllers
+{
+	//[Authorize("admin")]
+	public class NewsController : Controller
+	{
+
+		private readonly FileUploadService _fileService;
+		private readonly ApplicationDataContext _context;
+
+		public NewsController(FileUploadService fileUploadService, ApplicationDataContext context)
+		{
+			_fileService = fileUploadService;
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(AddNewsItemViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var pics = await _fileService.UploadFilesAsync(model.Pictures);
+				var newsItem = new NewsItem()
+				{
+					Created = DateTime.Now,
+					Id = Guid.NewGuid(),
+					Text = model.Text,
+					Title = model.Title,
+					Pictures = pics.ToList()
+				};
+				await _context.News.AddAsync(newsItem);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index", "Home");
+			}
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(Guid id)
+		{
+			var newsItem = await _context.News.FindAsync(id);
+			_context.News.Remove(newsItem);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("List", "News");
+		}
+	}
+}
---
+++ src/PortfolioApp.Site/Models/ViewModels/AddNewsItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Http;
+
+namespace PortfolioApp.Site.Models.ViewModels
+{
+	public class AddNewsItemViewModel
+	{
+		[Required(ErrorMessage = "Заголовок не указан")]
+		[Display(Name ="Заголовок")]
+		[DataType(DataType.Text)]
+		[MaxLength(64,ErrorMessage = "Длина заголовка должна быть не больше 64 символов")]
+		public string Title { get; set; }
+
+		[Required]
+		[Display(Name = "Текст новости")]
+		[DataType(DataType.MultilineText)]
+		public string Text { get; set; }
+
+		[Display(Name = "Изображения")]
+		[DataType(DataType.Upload)]
+		public List<IFormFile> Pictures { get; set; }
+
+		
+
+	}
+}
