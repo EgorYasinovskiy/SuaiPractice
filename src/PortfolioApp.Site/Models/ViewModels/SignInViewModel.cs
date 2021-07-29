619f344ede418424de9dad7d4257d52d0749ce33
Yasinovskiy Egor <yasinovskiy.egor1@gmail.com>
2021-07-29 06:13:24 +0300

    Добавил модели, в последующем используемые для Регистрастрации и авторизации пользователя

---
+++ src/PortfolioApp.Site/Models/ViewModels/SignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioApp.Site.Models.ViewModels
+{
+    public class SignInViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name ="Адрес электронной почты")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Пароль")]
+        public string Password { get; set; }    
+    }
+}
---
+++ src/PortfolioApp.Site/Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioApp.Site.Models.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        [Display(Name ="Адресс электронной почты")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name="Введите пароль")]
+        [DataType(DataType.Password)]
+        [MinLength(8,ErrorMessage ="Длина пароль должна быть минимум 8 символов")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Подтвердите пароль")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
